e753b4f36d095ff2565cab3b898bedbf4401b763
sudheeshcm <sudheeshm@qburst.com>
2017-08-01 16:21:15 +0530

    Added eslint rules for react and js files

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ class App extends Component {
           <h2>React Custom Form Builder</h2>
         </div>
         <p className="App-intro">
-          React based dynamic form builder application that lets you create a dynamic form with custom <code>buttons, text boxes</code> and other tools.
-          <br/>
-          App provides a drag and drop environment to style your own form with customizable properties for each tool you add to the form.
+          React based dynamic form builder application that lets you create a dynamic
+          form with custom <code>buttons, text boxes</code> and other tools.
+          <br />
+          App provides a drag and drop environment to style your own form with customizable
+          properties for each tool you add to the form.
         </p>
       </div>
     );
