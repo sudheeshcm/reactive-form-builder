b5a0f156b4b650baa0c4da0a8616cc3a99275ebe
sudheeshcm <sucm@zynga.com>
2023-07-13 21:22:07 +0530

    port update

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use(express.json());
 // Your static pre-build assets folder
 app.use(express.static(path.join(__dirname, '..', 'build')));
